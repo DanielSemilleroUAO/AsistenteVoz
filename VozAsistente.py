6300aee3ebc94e6313dbb2c4efeafeee81c38eb6
Daniel_Semillero <35827274+DanielSemilleroUAO@users.noreply.github.com>
2019-10-23 07:07:32 -0500

    Create VozAsistente.py

---
+++ VozAsistente.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 22 20:21:24 2019
+
+@author: daniel
+"""
+
+# importing speech recognition package from google api 
+import playsound # to play saved mp3 file 
+from gtts import gTTS # google text to speech 
+import os # to save/open files 
+  
+num = 1
+def assistant_speaks(output): 
+    global num 
+  
+    # num to rename every audio file  
+    # with different name to remove ambiguity 
+    num += 1
+    print("PerSon : ", output) 
+  
+    toSpeak = gTTS(text = output, lang ='es', slow = False) 
+    # saving the audio file given by google text to speech 
+    file = str(num)+".mp3"  
+    toSpeak.save(file) 
+      
+    # playsound package is used to play the same file. 
+    playsound.playsound(file, True)  
+    os.remove(file) 
+
+# Driver Code 
+if __name__ == "__main__": 
+    assistant_speaks("Papel, Desplazate dos lugares") 
+    assistant_speaks("Cartón") 
+    assistant_speaks("Plástico") 
+    assistant_speaks("Vidrio") 
+    assistant_speaks("Orgánico") 
